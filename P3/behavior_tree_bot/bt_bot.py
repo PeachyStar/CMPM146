674e84c8752312cbfc3a47eb942ce3fdf8577ac0
MayankhS <go2mayankh@gmail.com>
2026-01-26 15:36:18 -0800

    Update bt_bot.py
    First iteration of bot, only beats easy mode

--- P3/behavior_tree_bot/bt_bot.py
+++ P3/behavior_tree_bot/bt_bot.py
@@ -1,68 +1,66 @@
-#!/usr/bin/env python
-#
+import logging, sys, os
 
-"""
-// There is already a basic strategy in place here. You can use it as a
-// starting point, or you can throw it out entirely and replace it with your
-// own.
-"""
-import logging, traceback, sys, os, inspect
-logging.basicConfig(filename=__file__[:-3] +'.log', filemode='w', level=logging.DEBUG)
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
+# This adds the current directory to the path so it can find planet_wars.py
+# and the behavior_tree_bot folder.
+sys.path.insert(0, os.getcwd())
 
+from planet_wars import PlanetWars, finish_turn
 from behavior_tree_bot.behaviors import *
 from behavior_tree_bot.checks import *
 from behavior_tree_bot.bt_nodes import Selector, Sequence, Action, Check
 
-from planet_wars import PlanetWars, finish_turn
-
-
-# You have to improve this tree or create an entire new one that is capable
-# of winning against all the 5 opponent bots
 def setup_behavior_tree():
+    root = Selector(name='Main Strategy')
 
-    # Top-down construction of behavior tree
-    root = Selector(name='High Level Ordering of Strategies')
+    # Priority 1: Defense 
+    # Ensure 'defend_threatened_planet' exists in behaviors.py
+    defense = Sequence(name='Defense')
+    defense.child_nodes = [
+        Check(if_planet_threatened), 
+        Action(defend_threatened_planet)
+    ]
 
-    offensive_plan = Sequence(name='Offensive Strategy')
-    largest_fleet_check = Check(have_largest_fleet)
-    attack = Action(attack_weakest_enemy_planet)
-    offensive_plan.child_nodes = [largest_fleet_check, attack]
+    # Priority 2: Expansion
+    spread = Sequence(name='Spread')
+    spread.child_nodes = [
+        Check(if_neutral_planet_available), 
+        Action(spread_to_weakest_neutral_planet)
+    ]
 
-    spread_sequence = Sequence(name='Spread Strategy')
-    neutral_planet_check = Check(if_neutral_planet_available)
-    spread_action = Action(spread_to_weakest_neutral_planet)
-    spread_sequence.child_nodes = [neutral_planet_check, spread_action]
+    # Priority 3: Attack
+    attack = Sequence(name='Attack')
+    attack.child_nodes = [
+        Check(have_largest_fleet), 
+        Action(attack_weakest_enemy_planet)
+    ]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
-
-    logging.info('\n' + root.tree_to_string())
+    # Assemble the tree
+    root.child_nodes = [defense, spread, attack, Action(attack_weakest_enemy_planet)]
     return root
 
-# You don't need to change this function
-def do_turn(state):
-    behavior_tree.execute(planet_wars)
-
 if __name__ == '__main__':
-    logging.basicConfig(filename=__file__[:-3] + '.log', filemode='w', level=logging.DEBUG)
-
-    behavior_tree = setup_behavior_tree()
+    # Use an absolute path for the log to ensure it writes where you can see it
+    log_path = os.path.join(os.getcwd(), 'bt_bot.log')
+    logging.basicConfig(filename=log_path, filemode='w', level=logging.DEBUG)
+    
+    logging.info("Starting Bot...")
+    
     try:
+        behavior_tree = setup_behavior_tree()
+        logging.info("Tree Constructed Successfully:")
+        logging.info('\n' + behavior_tree.tree_to_string())
+
         map_data = ''
         while True:
-            current_line = input()
-            if len(current_line) >= 2 and current_line.startswith("go"):
-                planet_wars = PlanetWars(map_data)
-                do_turn(planet_wars)
-                finish_turn()
+            current_line = sys.stdin.readline()
+            if not current_line:
+                break 
+            if current_line.startswith("go"):
+                state = PlanetWars(map_data)
+                behavior_tree.execute(state)
+                finish_turn() # Sends "go" to Java [cite: 83, 85]
                 map_data = ''
             else:
-                map_data += current_line + '\n'
-
-    except KeyboardInterrupt:
-        print('ctrl-c, leaving ...')
-    except Exception:
-        traceback.print_exc(file=sys.stdout)
-        logging.exception("Error in bot.")
+                map_data += current_line
+    except Exception as e:
+        logging.exception("A fatal error occurred:")
