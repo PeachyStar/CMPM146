e55031823f40c442a3cbf14b94221ac977dbf498
MayankhS <go2mayankh@gmail.com>
2026-01-26 18:58:46 -0800

    Update checks.py FINAL

--- P3/behavior_tree_bot/checks.py
+++ P3/behavior_tree_bot/checks.py
@@ -9,3 +9,9 @@ def if_under_attack(state):
 def if_neutral_available(state):
     # Check if there are any planets not owned by anyone 
     return any(state.neutral_planets())
+
+def have_largest_fleet(state):
+    return sum(planet.num_ships for planet in state.my_planets()) \
+             + sum(fleet.num_ships for fleet in state.my_fleets()) \
+           > sum(planet.num_ships for planet in state.enemy_planets()) \
+             + sum(fleet.num_ships for fleet in state.enemy_fleets())
